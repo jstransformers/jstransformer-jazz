46af54af5cff2be00773a81ca66cbfdec4917cff
Rob Loach <robloach@gmail.com>
2015-10-16 22:17:33 -0400

    Fix async runner

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ var Promise = require('promise');
 exports.name = 'jazz';
 exports.outputFormat = 'html';
 
-exports.compileAsync = function (str, options) {
+exports.renderAsync = function (str, options, locals) {
   var template = jazz.compile(str, options);
-  return function(locals) {
-    return new Promise(function (resolve, reject) {
-      template.eval(locals, function (data) {
-        if (data) {
-          resolve(data);
-        }
-        else {
-          reject(data);
-        }
-      });
+  return new Promise(function (resolve, reject) {
+    template.eval(locals, function (data) {
+      if (data) {
+        resolve(data);
+      }
+      else {
+        reject(data);
+      }
     });
-  };
+  });
 };
